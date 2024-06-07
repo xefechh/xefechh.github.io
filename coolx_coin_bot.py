52d971e17bf82896dbfb1982d14e244528546eb5
xefechh <ponomarjov.gaabriel@gmail.com>
2024-06-07 08:40:52 -0700

    fullscreen

--- coolx_coin_bot.py
+++ coolx_coin_bot.py
@@ -1,5 +1,6 @@
 import telebot
 import webbrowser
+from telebot import types
 
 # Your bot's API token
 API_TOKEN = '7110404021:AAFJWA5d1CiAWTjROzKnLMfBbbXleDU5U_U'
@@ -7,5 +8,8 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.username}')
+    print(message.from_user.first_name)
+
+
 bot.polling(non_stop=True)
