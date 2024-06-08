ddc6812a3264fb93548b348f5c6af37ab7075733
xefechh <ponomarjov.gaabriel@gmail.com>
2024-06-08 13:38:36 -0700

    coin upgrate

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let score = 0;
 
     const scoreDisplay = document.getElementById('score');
-    const clickButton = document.getElementById('main_click_button');
+    const clickButton = document.getElementById('main-coin')
 
     clickButton.addEventListener('click', () => {
         score++;
-        scoreDisplay.textContent = `Score: ${score}cc`;
+        scoreDisplay.textContent = `${score}`;
     });
 });
