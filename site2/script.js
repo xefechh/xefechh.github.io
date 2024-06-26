e52a2a63eaa8e0c2a9571e6582f18c0e03913328
xefechh <168400960+xefechh@users.noreply.github.com>
2024-06-26 11:29:41 -0700

    Add files via upload

---
+++ site2/script.js
@@ -0,0 +1,11 @@
+const screen = document.querySelector('.screen');
+const green = document.querySelector('.data-field');
+
+green.addEventListener('click', (event) => {
+    event.stopPropagation();
+});
+
+screen.addEventListener('click', () => {
+    console.log("Click")
+});
+
