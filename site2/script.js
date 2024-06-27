4ad461b9be5170264da22b1637489bcd4ee248db
xefechh <ponomarjov.gaabriel@gmail.com>
2024-06-27 03:49:46 -0700

    Numnber clicking

--- site2/script.js
+++ site2/script.js
@@ -3,14 +3,12 @@ window.Telegram.WebApp.expand();
 const click_area = document.querySelector('.screen');
 const card_area = document.querySelector('.card');
 const money = document.getElementById('money');
-
+let score = 0;
 card_area.addEventListener('click', (event) => {
     event.stopPropagation();
 });
 
-screen.addEventListener('click', () => {
-    console.log("Click")
+click_area.addEventListener('click', () => {
+    score++;
+    money.innerHTML = `${score}`;
 });
-
-
-
