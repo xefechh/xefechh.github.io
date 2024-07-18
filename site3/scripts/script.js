b0b186a310a21f8a47322dc6932376e2456398a2
xefechh <ponomarjov.gaabriel@gmail.com>
2024-07-18 14:38:56 -0700

    clickable

--- site3/scripts/script.js
+++ site3/scripts/script.js
@@ -1,1 +1,11 @@
 window.Telegram.WebApp.expand();
+
+const budget = document.querySelector('.budget');
+let score = 0;
+
+document.body.addEventListener('pointerdown', (event) => {
+  if (!event.target.closest('.card, .data, .progressbar')) {
+    score++;
+    budget.innerHTML = `<i class='bx bx-dollar'></i>${score}`;
+  }
+});
