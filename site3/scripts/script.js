30febd47dac784c8f71a158df8f39ff1b504d7d4
xefechh <ponomarjov.gaabriel@gmail.com>
2024-07-19 06:09:59 -0700

    Update script.js

--- site3/scripts/script.js
+++ site3/scripts/script.js
@@ -4,7 +4,7 @@ const budget = document.querySelector('.budget');
 let score = 0;
 
 document.body.addEventListener('pointerdown', (event) => {
-  if (!event.target.closest('.card, .data, .progressbar')) {
+  if (!event.target.closest('.card, .data, .progressbar, .navbar')) {
     score++;
     budget.innerHTML = `<i class='bx bx-dollar'></i>${score}`;
   }
