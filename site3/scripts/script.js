a71a2d2327d99d77fb308a160ac801eea1fec67c
xefechh <ponomarjov.gaabriel@gmail.com>
2024-07-20 22:11:17 +0300

    progressbar updating

--- site3/scripts/script.js
+++ site3/scripts/script.js
@@ -7,5 +7,17 @@ document.body.addEventListener('pointerdown', (event) => {
   if (!event.target.closest('.card, .data, .progressbar, .navbar')) {
     score++;
     budget.innerHTML = `<i class='bx bx-dollar'></i>${score}`;
+    updateProgressBar();
   }
 });
+
+function updateProgressBar() {
+  const progressBar = document.querySelector('.progress');
+  const nextLevelValue = parseInt(document.querySelector('.next-level').textContent);
+
+  // Calculate the progress (assuming the value is a number)
+  const progress = (score / nextLevelValue) * 100;
+
+  // Update the progress bar's width
+  progressBar.style.width = `${progress}%`;
+}
