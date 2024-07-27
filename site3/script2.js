b6abdf64a86ce7c5e24f6253be757c0830a988a1
xefechh <ponomarjov.gaabriel@gmail.com>
2024-07-27 22:24:35 +0300

    webapp

--- site3/script2.js
+++
@@ -1,6 +0,0 @@
-const wapp = window.Telegram.WebApp;
-wapp.expand();  
-
-setTimeout(() => {
-    window.location.href = 'home.html'; // Redirect to home.html
-}, 3000); // 3000 milliseconds = 3 seconds
