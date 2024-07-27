e3224da6074567202fd1fbf1d9572f95a56268be
xefechh <ponomarjov.gaabriel@gmail.com>
2024-07-28 00:14:49 +0300

    neapp

--- site4/script.js
+++ site4/script.js
@@ -8,10 +8,16 @@ let pertap = document.querySelector('.pertap');
 let perhour = document.querySelector('.perhour');
 
 document.body.addEventListener('pointerdown', (event) => {
-  if (event.target.matches('.coin')) {
+  if (!event.target.matches('.card, .data, .progressbar, .navbar')) {
     score++;
     budget.innerHTML = `<i class='bx bx-dollar'></i>${score}`;
     updateProgressBar();
   }
 });
 
+function updateProgressBar() {
+  const progressBar = document.querySelector('.progress');
+  const nextLevelValue = parseInt(document.querySelector('.next-level').textContent);
+  const progress = (score / nextLevelValue) * 100;
+  progressBar.style.width = `${progress}%`;
+}
